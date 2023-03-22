d3855a29ea20b63e93fafb8f3ab7d152cfe1ea46
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-03-22 00:14:15 +0000

    RuntimeBinderException

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,6 +4,8 @@ using static System.Console;
 OutputTimeStaticBinding();
 WriteLine("==================================");
 OutputTimeDynamicBinding();
+WriteLine("==================================");
+OutputDynamicRuntimeError();
 
 void OutputTimeStaticBinding()
 {
@@ -16,5 +18,14 @@ void OutputTimeDynamicBinding()
 {
     dynamic dt = DateTime.Now;
     string time = dt.ToLongTimeString();
+    WriteLine(time);
+}
+
+void OutputDynamicRuntimeError()
+{
+    dynamic dt = DateTime.Now;
+    
+    // This will throw a RuntimeBinderException:
+    string time = dt.IsItCoffeeTime();
     WriteLine(time);
 }

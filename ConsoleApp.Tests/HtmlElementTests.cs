b8bfd1eb021b4e151208dbb2561ec37ca96038ff
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-03-23 02:04:56 +0000

    Custom Dynamic Classes: Getting dynamic member names

--- ConsoleApp.Tests/HtmlElementTests.cs
+++ ConsoleApp.Tests/HtmlElementTests.cs
@@ -16,4 +16,17 @@ public class HtmlElementTests
         image.src = "car.jpg";
         Assert.Equal("car.jpg", image.src);
     }
+    
+    [Fact]
+    public void ShouldReturnDynamicMemberNames()
+    {
+        dynamic image = new HtmlElement("img");
+        image.src = "car.jpg";
+        image.alt = "Car";
+
+        string[] members = image.GetDynamicMemberNames();
+        // Assert.Equal(new[] {"src", "alt"}, members);
+        Assert.Equal("src", members[0]);
+        Assert.Equal("alt", members[1]);
+    }
 }

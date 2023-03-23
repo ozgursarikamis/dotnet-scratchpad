14e7d709cb2f508c6f3b69bf377d847a76a6038e
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-03-23 01:51:20 +0000

    Custom Dynamic Classes: Why we need `dynamic`

--- ConsoleApp.Tests/HtmlElementTests.cs
+++ ConsoleApp.Tests/HtmlElementTests.cs
@@ -8,4 +8,13 @@ public class HtmlElementTests
         var image = new HtmlElement("img");
         Assert.Equal("img", image.TagName);
     }
+
+    [Fact]
+    public void ShouldAddAttributeNameAndValueDynamically()
+    {
+        // Won't even compile
+        var image = new HtmlElement("img");
+        image.src = "car.jpg";
+        Assert.Equal("car.jpg", image.src);
+    }
 }

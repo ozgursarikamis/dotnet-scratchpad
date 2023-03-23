c292ea88c5a5da39178f1becfea35965d6525149
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-03-23 02:14:49 +0000

    Custom Dynamic Classes: Implementing Additional Interfaces (`IDictionary<,>` interface used)

--- ConsoleApp/HtmlElement.cs
+++ ConsoleApp/HtmlElement.cs
@@ -1,9 +1,10 @@
+using System.Collections;
 using System.Dynamic;
 using System.Text;
 
 namespace ConsoleApp;
 
-public class HtmlElement : DynamicObject
+public class HtmlElement : DynamicObject, IDictionary<string, object>
 {
     private readonly Dictionary<string, object?> _attributes = new();
     public string TagName { get; set; }
@@ -34,6 +35,11 @@ public class HtmlElement : DynamicObject
         return _attributes.Keys.ToArray();
     }
 
+    public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
     public override string ToString()
     {
         var sb = new StringBuilder();
@@ -48,4 +54,65 @@ public class HtmlElement : DynamicObject
 
         return sb.ToString();
     }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Add(KeyValuePair<string, object> item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(KeyValuePair<string, object> item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CopyTo(KeyValuePair<string, object>[] array, int arrayIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(KeyValuePair<string, object> item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Count { get; }
+    public bool IsReadOnly { get; }
+    public void Add(string key, object value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool ContainsKey(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool TryGetValue(string key, out object value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object this[string key]
+    {
+        get => throw new NotImplementedException();
+        set => throw new NotImplementedException();
+    }
+
+    public ICollection<string> Keys { get; }
+    public ICollection<object> Values { get; }
 }

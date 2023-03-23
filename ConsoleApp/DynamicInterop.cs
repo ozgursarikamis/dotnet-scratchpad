8baa3c4dfa9c454d7712943fd5a6a90415c18d80
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-03-23 23:22:15 +0000

    Dynamic Interop: Running Python script in C#

--- ConsoleApp/DynamicInterop.cs
+++ ConsoleApp/DynamicInterop.cs
@@ -1,4 +1,5 @@
 using IronPython.Hosting;
+using Microsoft.Scripting;
 using Microsoft.Scripting.Hosting;
 
 namespace ConsoleApp;
@@ -9,12 +10,17 @@ public static class DynamicInterop
     {
         // create a python engine:
         ScriptEngine engine = Python.CreateEngine();
-        string simpleExpression = "1 + 2";
-        dynamic dynamicResult = engine.Execute(simpleExpression);
         
-        // int typedResult = engine.Execute<int>(simpleExpression);
-        // WriteLine(typedResult);
+        int customerAge = 42;
+        WriteLine("Enter an expression");
+        string expression = ReadLine();
         
+        ScriptScope scope = engine.CreateScope();
+        scope.SetVariable("a", customerAge);
+        
+        ScriptSource source = engine.CreateScriptSourceFromString(expression, SourceCodeKind.Expression);
+        dynamic dynamicResult = source.Execute(scope);
+
         WriteLine($"Expression Result: {dynamicResult}");
         
         WriteLine("Press any key to continue...");

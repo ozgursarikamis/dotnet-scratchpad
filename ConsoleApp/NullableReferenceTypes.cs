7efec534690db511704fab637c58e456e7d1a6cb
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-03-24 00:11:08 +0000

    Nullable Reference Types: Project classes

--- ConsoleApp/NullableReferenceTypes.cs
+++ ConsoleApp/NullableReferenceTypes.cs
@@ -4,8 +4,7 @@ public static class NullableReferenceTypes
 {
     public static void Run()
     {
-        BlogPost post = null;
-        PrintTitle(post);
+        
     }
 
     private static void PrintTitle(BlogPost post)
@@ -16,18 +15,18 @@ public static class NullableReferenceTypes
 
 public class BlogPost
 {
-    public string Title { get; set; }
-    public List<Comment> Comments { get; set; }
+    public string? Title { get; set; }
+    public List<Comment>? Comments { get; set; }
 }
 
 public class Author
 {
-    public string Name { get; set; }
-    public string Email { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
 }
 
 public class Comment
 {
-    public string Body { get; set; }
-    public Author Author { get; set; }
+    public string? Body { get; set; }
+    public Author? PostedBy { get; set; }
 }

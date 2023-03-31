e500e39afa8c1f748d01929b7bf89eb34b891484
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-03-31 02:20:25 +0100

    [BlockingCollection] Take method

--- CSharp.ConcurrentCollections/BlockingCollections.cs
+++ CSharp.ConcurrentCollections/BlockingCollections.cs
@@ -6,10 +6,25 @@ public static class BlockingCollections
 {
     public static void Run()
     {
-        GenerateABlockingCollection();
+        var collection = GenerateABlockingCollection();
+        RemovingItemsFromABlockingCollection();
     }
 
-    private static void GenerateABlockingCollection()
+    private static void RemovingItemsFromABlockingCollection()
+    {
+        var collection = new BlockingCollection<int>
+        {
+            10, 20
+        };
+        foreach (var i in collection)
+            WriteLine($"Item: {i}");
+
+        WriteLine("==================================== TAKE METHOD:");
+        var result1 = collection.Take();
+        WriteLine("Item: {0}", result1);
+    }
+
+    private static BlockingCollection<int> GenerateABlockingCollection()
     {
         BlockingCollection<int> collection = new(4);
         collection.Add(10);
@@ -31,5 +46,7 @@ public static class BlockingCollections
         {
             WriteLine(item);
         }
+
+        return collection;
     }
 }

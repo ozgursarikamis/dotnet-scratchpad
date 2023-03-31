1a0e52c858f2f31b62490eb332c8217c650b9662
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-03-31 02:24:19 +0100

    [BlockingCollection] TryTake method

--- CSharp.ConcurrentCollections/BlockingCollections.cs
+++ CSharp.ConcurrentCollections/BlockingCollections.cs
@@ -22,6 +22,10 @@ public static class BlockingCollections
         WriteLine("==================================== TAKE METHOD:");
         var result1 = collection.Take();
         WriteLine("Item: {0}", result1);
+        
+        WriteLine("==================================== TryTake METHOD WITH TIMEOUT:");
+        var tryTake = collection.TryTake(out var result2, TimeSpan.FromSeconds(1));
+        WriteLine("Item: {0}", tryTake ? result2.ToString() : "No item was available to be removed");
     }
 
     private static BlockingCollection<int> GenerateABlockingCollection()

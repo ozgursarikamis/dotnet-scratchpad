d4be3c43bfe02479807941e3e21fa3f484ceb2c8
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-04-02 03:13:53 +0100

    [GLOBALIZATION]: Standard Numeric Formats

--- Framework/Globalization/Globalization/WorkingWithNumbers.cs
+++ Framework/Globalization/Globalization/WorkingWithNumbers.cs
@@ -4,16 +4,36 @@ public static class WorkingWithNumbers
 {
     public static void Run()
     {
+        StandardNumericFormatStrings();
         DisplayNumbersAsStrings();
+    }
+
+    /// <summary>
+    ///     Standard Numeric Format Strings
+    /// </summary>
+    private static void StandardNumericFormatStrings()
+    {
+        var culture = new CultureInfo("en-US");
+        const double amount = 1500.5;
+        WriteLine($"Currency: {amount.ToString("C", culture)}"); // $1,500.50
+        WriteLine($"Exponential: {amount.ToString("E", culture)}"); // 1.500500E+003
+        WriteLine($"Fixed point: {amount.ToString("F", culture)}"); // 1500.50
+        WriteLine($"General: {amount.ToString("G", culture)}"); // 1500.5
+        WriteLine($"Number: {amount.ToString("N", culture)}"); // 1,500.50
+        WriteLine($"Percent: {amount.ToString("P", culture)}"); // 150,050.00 %
+        WriteLine($"Round-trip: {amount.ToString("R", culture)}"); // 1500.5
+        WriteLine($"Hexadecimal: {255.ToString("X", culture)}"); // 5DC.8000000000003
+        WriteLine($"Decimal: {1500.ToString("D", culture)}"); // 150050
+        WriteLine();
     }
 
     private static void DisplayNumbersAsStrings()
     {
-        
         var culture = CultureInfo.CreateSpecificCulture("sv-SE");
         decimal number = 1_000_000.50m;
         string numberAsString = number.ToString(culture);
         
-        WriteLine(numberAsString);
+        WriteLine($"Number As String: {numberAsString}");
+        WriteLine();
     }
 }

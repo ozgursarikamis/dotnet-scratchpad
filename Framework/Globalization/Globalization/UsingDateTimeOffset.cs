5a92c054fbea000860ae4b8a7e2e8978392e745a
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-04-03 03:22:10 +0100

    [GLOBALIZATION]: Localized Time Formats

--- Framework/Globalization/Globalization/UsingDateTimeOffset.cs
+++ Framework/Globalization/Globalization/UsingDateTimeOffset.cs
@@ -4,7 +4,20 @@ public static class UsingDateTimeOffset
 {
     public static void Run()
     {
+        ParsingDatesFromStrings();
         ConvertToLocalDateFromUtc();
+    }
+
+    private static void ParsingDatesFromStrings()
+    {
+        DateTimeOffset date = new DateTimeOffset(2021, 05, 10, 18, 30, 59, TimeSpan.FromHours(8));
+        string dateAsString = date.ToString("F", new CultureInfo("sv-SE"));
+        string dateAsString2 = date.ToString("F", new CultureInfo("in-IN"));
+        string dateAsString3 = date.ToString("F", new CultureInfo("ar-SA"));
+        WriteLine($"SE: {dateAsString} ");
+        WriteLine($"IN: {dateAsString2} ");
+        WriteLine($"AR: {dateAsString3} ");
+        WriteLine("========================================");
     }
 
     private static void ConvertToLocalDateFromUtc()
